2c651408ba265516d76ceef91242cc78256e10e9
harabalos <babismetaxas000@gmail.com>
2023-02-12 20:10:55 +0200

    added comments to the rectangle class

--- Rectangle.java
+++ Rectangle.java
@@ -1,9 +1,11 @@
 public class Rectangle {
+    // instance variables to store the minimum x, y and maximum x, y coordinates of the rectangle
     private int xmin;
     private int ymin;
     private int xmax;
     private int ymax;
 
+    // constructor to initialize the minimum and maximum x, y coordinates of the rectangle
     public Rectangle(int xmin, int ymin, int xmax, int ymax) {
         this.xmin = xmin;
         this.ymin = ymin;
@@ -11,31 +13,38 @@ public class Rectangle {
         this.ymax = ymax;
     }
 
+    // method to return the minimum x-coordinate of the rectangle
     public int xmin() {
         return xmin;
     }
 
+    // method to return the minimum y-coordinate of the rectangle
     public int ymin() {
         return ymin;
     }
 
+    // method to return the maximum x-coordinate of the rectangle
     public int xmax() {
         return xmax;
     }
 
+    // method to return the maximum y-coordinate of the rectangle
     public int ymax() {
         return ymax;
     }
 
+    // method to determine if the point p is inside or on the sides of the rectangle
     public boolean contains(Point p) {
         return p.x() >= xmin && p.x() <= xmax && p.y() >= ymin && p.y() <= ymax;
     }
 
+    // method to determine if the two rectangles intersect
     public boolean intersects(Rectangle that) {
         return this.xmax >= that.xmin && this.xmin <= that.xmax &&
                this.ymax >= that.ymin && this.ymin <= that.ymax;
     }
 
+    // method to calculate the square of the Euclidean distance from a point p to the closest point in the rectangle
     public int squareDistanceTo(Point p) {
         int x = Math.max(Math.min(p.x(), xmax), xmin);
         int y = Math.max(Math.min(p.y(), ymax), ymin);
@@ -44,6 +53,7 @@ public class Rectangle {
         return xDiff * xDiff + yDiff * yDiff;
     }
 
+    // method to return a string representation of the rectangle in the format [xmin, xmax] x [ymin, ymax]
     public String toString() {
         return "[" + xmin + ", " + xmax + "] x [" + ymin + ", " + ymax + "]";
     }
